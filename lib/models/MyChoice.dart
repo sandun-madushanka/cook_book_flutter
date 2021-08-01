6b152b229c17531f5035c953db5315a36ae88cff
Sandun Madushanka <sandundematagoda@gmail.com>
2021-08-01 14:10:12 +0530

    sample model for essential custome widget

---
+++ lib/models/MyChoice.dart
@@ -0,0 +1,11 @@
+class MyChoice {
+  int index;
+  String choice, deliveryNote;
+  double deliveryCharge;
+
+  MyChoice(
+      {required this.index,
+      required this.choice,
+      required this.deliveryNote,
+      required this.deliveryCharge});
+}
